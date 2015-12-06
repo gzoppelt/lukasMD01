c733022e9079830219a92bf9a25ea5e5316ee009
Guenther <guenther.zoppelt@gmail.com>
2015-12-06 10:06:56 +0100

    v0.1 to1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lukasMD01
 Just enough Angular for Designers
+
+<pre><code>
+$ npm install -g serve
+
+</code></pre>
---
+++ t01/scripts.js
@@ -0,0 +1,27 @@
+/**
+ * Created by g on 04/12/15.
+ */
+var app = angular.module('myApp', ['ngMaterial']);
+
+app.controller('MainController', function (){
+    this.items = [
+        {
+            label: 'Item One',
+            description: 'I am in an object boat!',
+            price: 100,
+            url: 'http://angularjs.org'
+        },
+        {
+            label: 'Item Two',
+            description: 'I am cool!',
+            price: 200,
+            url: 'http://angularjs.org'
+        },
+        {
+            label: 'Item Three',
+            description: 'I am much smarter!',
+            price: 300,
+            url: 'http://angularjs.org'
+        }
+    ];
+});
